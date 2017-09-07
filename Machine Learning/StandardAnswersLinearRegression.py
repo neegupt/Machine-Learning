3394128cbd5c74c13128ea6980238f133f3324f0
neegupt <31516905+neegupt@users.noreply.github.com>
2017-09-07 14:12:14 -0700

    Add files via upload

---
+++ Machine Learning/StandardAnswersLinearRegression.py
@@ -0,0 +1,4 @@
+1. I expect to see the coefficient as around .3 and the intercept as around 1. This would demonstrate that the regression works because the line is approximating the data. 
+2. I expect to see a best fit line, that cuts through the middle of the y values.
+3. I could remove the noise from the equation, and then the numbers the regression outputs should be identical to the equation Jen put in. 
+4. My guess still holds true with the 3D dataset, because the 3D dataset outputs two coeffecients because there are two different inputs for each y value. 
