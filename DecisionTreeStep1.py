070bc5eee209bfeeffd35898fe4a4b0a67223935
neegupt <31516905+neegupt@users.noreply.github.com>
2017-09-19 20:44:58 -0700

    Add files via upload

---
+++ DecisionTreeStep1.py
@@ -0,0 +1,17 @@
+from random import randint
+
+data = []
+for j in range (0,10):
+    new_list = []
+    for i in range (0,100):
+        new_list.append(randint(0,1))
+    data.append(new_list)
+print (data)
+
+
+for k in range(
+for i in range (0,100):
+    for j in range (0,10):
+        if data[j][i]==1 and if data[j][0]==1
+    
+
