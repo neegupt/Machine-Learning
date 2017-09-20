35cefbfa35e3740126190328df04cf8e1971c475
neegupt <31516905+neegupt@users.noreply.github.com>
2017-09-19 20:46:07 -0700

    Update DecisionTreeStep1.py

--- DecisionTreeStep1.py
+++ DecisionTreeStep1.py
@@ -1,9 +1,9 @@
 from random import randint
 
 data = []
-for j in range (0,10):
+for j in range (0,100):
     new_list = []
-    for i in range (0,100):
+    for i in range (0,10):
         new_list.append(randint(0,1))
     data.append(new_list)
 print (data)
