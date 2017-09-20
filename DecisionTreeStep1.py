0ffd50463b038f2522bc4e828e6ecb3cb9bcca8f
tylkahn <tylkahn@nuevaschool.org>
2017-09-20 13:38:04 -0700

    Update DecisionTreeStep1.py

--- DecisionTreeStep1.py
+++ DecisionTreeStep1.py
@@ -4,13 +4,13 @@ import random
 n=10
 
 data = []
-random.seed("SamTopNeelBottom")
+# random.seed("SamTopNeelBottom")
 for j in range (0,100):
     new_list = []
     for i in range (0,n):
         new_list.append(randint(0,1))
     data.append(new_list)
-print (data)
+# print (data)
 
 
 featurestrength = [0,0,0,0,0,0,0,0,0,0]
@@ -22,19 +22,15 @@ for j in range (0,100):
 
 for i in range (1,n):
     featurestrength[i] = abs(50-featurestrength[i])
-for i in range(1,n):
-    for j in range(1,n):
-        if featurestrength[i]>featurestength[j]:
-            optimalfeature=i
-datasplit_1_1 = []  
-datasplit_1_2 = []
-for i in range(0,n):
-    if data[i][optimalfeature]=1:
+
+optimalfeature = featurestrength.index(max(featurestrength)
+ 
+datasplit_1 = []
+datasplit_2 = []
+
+for i in range(0,100):
+    if data[i][optimalfeature] == 1:
         datasplit_1_1.append[data[i]]
-    if else:
+    else:
         datasplit_1_2.append[data[i]]
         
-        
-    
-
-
