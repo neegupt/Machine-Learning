572e72d056fdf44f51691e660b514cc20384ae7b
tylkahn <tylkahn@nuevaschool.org>
2017-09-20 14:20:18 -0700

    Update DecisionTreeStep1.py

--- DecisionTreeStep1.py
+++ DecisionTreeStep1.py
@@ -3,6 +3,8 @@ import random
 
 n=10
 
+Depth=5
+
 data = []
 # random.seed("SamTopNeelBottom")
 for j in range (0,100):
@@ -12,25 +14,40 @@ for j in range (0,100):
     data.append(new_list)
 # print (data)
 
+def strength(points, level):
+	if level>=Depth:
+		print classes
+		break
+	featurestrength = [0,0,0,0,0,0,0,0,0,0]
 
-featurestrength = [0,0,0,0,0,0,0,0,0,0]
+	for j in range (0,len(points)):
+	    for i in range(1,n):
+	        if (data[j][i]==1 and data[j][0]==1) or (data[j][i]==0 and data[j][0]==0):
+	            featurestrength[i]+=1
 
-for j in range (0,100):
-    for i in range(1,n):
-        if (data[j][i]==1 and data[j][0]==1) or (data[j][i]==0 and data[j][0]==0):
-            featurestrength[i]+=1
+	featurestrength2 = featurestrength
 
-for i in range (1,n):
-    featurestrength[i] = abs(50-featurestrength[i])
+	optimalfeature = [abs(50-x) for x in featurestrength[1:]].index(max([abs(50-x) for x in featurestrength[1:]]))+1
 
-optimalfeature = featurestrength.index(max(featurestrength)
- 
-datasplit_1 = []
-datasplit_2 = []
+	# print featurestrength[1:]
+	# sort stuff here
+	# datapoints = subsets of data here
 
-for i in range(0,100):
-    if data[i][optimalfeature] == 1:
-        datasplit_1_1.append[data[i]]
-    else:
-        datasplit_1_2.append[data[i]]
-        
+	level+=1
+	for datapoint in datapoints:
+		strength(datapoint,level)
+
+
+strength(data,0)
+
+	# return featurestrength[optimalfeature]
+
+
+
+# if optimalfeature>50:
+	
+# elif optimalfeature<50:
+# 	jgifdkjsg
+# else:
+# 	continue
+
