29fa6386d412a6ce4246de34ab3e3ceb594084ac
tylkahn <tylkahn@nuevaschool.org>
2017-09-19 21:43:09 -0700

    Update DecisionTreeStep1.py

--- DecisionTreeStep1.py
+++ DecisionTreeStep1.py
@@ -11,14 +11,14 @@ for j in range (0,100):
 print (data)
 
 
-featurestrength = [0,0,0,0,0,0,0,0,0]
+featurestrength = [0,0,0,0,0,0,0,0,0,0]
 
 for j in range (0,100):
     for i in range(1,10):
         if (data[j][i]==1 and data[j][0]==1) or (data[j][i]==0 and data[j][0]==0):
-            featurestrength[i-1]+=1
+            featurestrength[i]+=1
 
-for i in range (0,9):
+for i in range (1,10):
     featurestrength[i] = abs(50-featurestrength[i])
     
 list1, list2 = (list(x) for x in zip(*sorted(zip(featurestrength, [1,2,3,4,5,6,7,8,9]))))
