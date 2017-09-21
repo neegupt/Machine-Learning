2b710749ae3936aab04b706541a4a746ab924de6
neegupt <31516905+neegupt@users.noreply.github.com>
2017-09-21 11:54:23 -0700

    Update DecisionTreeStep1.py

--- DecisionTreeStep1.py
+++ DecisionTreeStep1.py
@@ -13,7 +13,7 @@ for j in range (0,100):
     for i in range (0,n):
         new_list.append(randint(0,1))
     data.append(new_list)
-# print (data)
+
 
 final = []
 order = []
@@ -27,12 +27,13 @@ def strength(points, level, exclude):
     else:
         featurestrength = [0,0,0,0,0,0,0,0,0,0]
 
-      	print ("points", points)
+        
+
 
         for j in range (0,len(points)):
             for i in range(1,n):
-            	# check if feature has been used in branch before
-            	print j,i
+                # check if feature has been used in branch before
+                
                 if i in excluded:
                     featurestrength[i]=50
                 elif (points[j][i]==1 and points[j][0]==1) or (points[j][i]==0 and points[j][0]==0):
@@ -41,7 +42,7 @@ def strength(points, level, exclude):
         optimalfeature = [abs(50-x) for x in featurestrength[1:]].index(max([abs(50-x) for x in featurestrength[1:]]))+1
         datapoint_0=[]
         datapoint_1=[]
-        print("optimal", optimalfeature)
+        
         for i in range (0,len(points)):
             if points[i][optimalfeature] == 0:
                 datapoint_0.append(points[i])
@@ -51,7 +52,7 @@ def strength(points, level, exclude):
         excluded.append(optimalfeature)
 
         order.extend([optimalfeature, featurestrength[optimalfeature]])
-        print ("exclude", exclude)
+       
 
         # run the function on the two new subgroups
         strength(datapoint_0, level+1, excluded)
@@ -62,6 +63,6 @@ def strength(points, level, exclude):
 
 
 strength(data,0,[])
-print final
+
 
 print order
