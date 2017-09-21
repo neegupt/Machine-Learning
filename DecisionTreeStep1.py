a666a6999c6a5eb53ca791273be2cf66350da084
neegupt <31516905+neegupt@users.noreply.github.com>
2017-09-20 21:18:10 -0700

    Update DecisionTreeStep1.py

--- DecisionTreeStep1.py
+++ DecisionTreeStep1.py
@@ -18,61 +18,49 @@ for j in range (0,100):
 final = []
 order = []
 def strength(points, level, exclude):
-	excluded = []
-	excluded.extend(exclude)
-	if level>=Depth:
-		final.append([points])
-	else:
-		featurestrength = [0,0,0,0,0,0,0,0,0,0]
+    excluded = []
+    excluded.extend(exclude)
+    if level>=Depth:
+        final.append([points])
+    else:
+        featurestrength = [0,0,0,0,0,0,0,0,0,0]
 
-		for j in range (0,len(points)):
-		    for i in range(1,n):
-		    	if i in excluded:
-		    		featurestrength[i]=50
-		        elif (data[j][i]==1 and data[j][0]==1) or (data[j][i]==0 and data[j][0]==0):
-		            featurestrength[i]+=1
-
-
-		optimalfeature = [abs(50-x) for x in featurestrength[1:]].index(max([abs(50-x) for x in featurestrength[1:]]))+1
-		datapoint_1=[]
-		datapoint_2=[]
-		print("optimal", optimalfeature)
-		for i in range (0,100):
-			if data[i][optimalfeature] == 0:
-				datapoint_1.append([data[i]])
-			else:
-				datapoint_2.append([data[i]])
-
-		excluded.append(optimalfeature)
-
-		order.extend([optimalfeature, featurestrength[optimalfeature]])
-		print ("exclude", exclude)
+        for j in range (0,len(points)):
+            for i in range(1,n):
+                if i in excluded:
+                    featurestrength[i]=50
+                elif (data[j][i]==1 and data[j][0]==1) or (data[j][i]==0 and data[j][0]==0):
+                    featurestrength[i]+=1
 
 
-		strength(datapoint_1, level+1, excluded)
-		exclude.append(optimalfeature)
-		excluded = exclude
-		strength(datapoint_2, level+1, excluded)
-	
+        optimalfeature = [abs(50-x) for x in featurestrength[1:]].index(max([abs(50-x) for x in featurestrength[1:]]))+1
+        datapoint_1=[]
+        datapoint_2=[]
+        print("optimal", optimalfeature)
+        for i in range (0,100):
+            if data[i][optimalfeature] == 0:
+                datapoint_1.append([data[i]])
+            else:
+                datapoint_2.append([data[i]])
 
-	# print featurestrength[1:]
-	# sort stuff here
-	# datapoints = subsets of data here
+        excluded.append(optimalfeature)
+
+        order.extend([optimalfeature, featurestrength[optimalfeature]])
+        print ("exclude", exclude)
+
+
+        strength(datapoint_1, level+1, excluded)
+        exclude.append(optimalfeature)
+        excluded = exclude
+        strength(datapoint_2, level+1, excluded)
+    
 
 
 strength(data,0,[])
 print final
+
 print order
 
 
 
-	# return featurestrength[optimalfeature]
-
-
-
-# if optimalfeature>50:
-	
-# elif optimalfeature<50:
-# 	jgifdkjsg
-# else:
-# 	continue
+   
